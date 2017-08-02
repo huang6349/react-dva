e401a6159d618b9990578e7030b513d423e1ce42
huang6349 <252683858@qq.com>
2017-08-02 14:21:17 +0800

    设计主布局

--- src/routes/MainLayout.js
+++ src/routes/MainLayout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
   Layout,
+  Breadcrumb,
 } from 'antd';
 import PropTypes from 'prop-types';
 import { connect } from 'dva';
@@ -16,12 +17,25 @@ class MainLayout extends React.PureComponent {
 
   render() {
     return (
-      <Layout>
-        <Layout.Sider></Layout.Sider>
+      <Layout className={styles['ant-layout']}>
+        <Layout.Sider
+          collapsible={true}
+          defaultCollapsed={false}
+          width="224"
+          collapsedWidth="64"
+          className={styles['ant-layout-sider']}>
+        </Layout.Sider>
         <Layout>
-          <Layout.Header></Layout.Header>
-          <Layout.Content children={this.props.children}></Layout.Content>
-          <Layout.Footer></Layout.Footer>
+          <Layout.Header className={styles['ant-layout-header']}></Layout.Header>
+          <Layout.Header className={styles['ant-layout-breadcrumb']}>
+            <Breadcrumb>
+              <Breadcrumb.Item>首页</Breadcrumb.Item>
+            </Breadcrumb>
+          </Layout.Header>
+          <Layout className={styles['ant-layout-main']}>
+            <Layout.Content children={this.props.children}></Layout.Content>
+            <Layout.Footer className={styles['ant-layout-footer']}>Ant&nbsp;Design&nbsp;版权所有&nbsp;©&nbsp;2015&nbsp;由蚂蚁金服体验技术部支持</Layout.Footer>
+          </Layout>
         </Layout>
       </Layout>
     );
