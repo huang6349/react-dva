07ed0e7e2cc58e352c0ee2f4e371b806de59f629
huang6349 <252683858@qq.com>
2017-08-10 10:28:55 +0800

    增加常量类

---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+// 系统常量
+
+// 菜单
+const MENU = {
+  ID: 'id',
+  PARENT_ID: 'parent_id',
+  NAME: 'name',
+  ICON: 'icon',
+  URL: 'url',
+  CHILD: 'childs',
+  PID: null,
+};
+
+export default {
+  MENU: MENU,
+}
